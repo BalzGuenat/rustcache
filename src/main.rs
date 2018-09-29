19392055e26eb79d3af639c5c39968cf038f055c
Balz Guenat <balz.guenat@gmail.com>
2018-09-29 14:10:45 +0200

    multithreading!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused_imports)]
 
+extern crate threadpool;
+
 use std::str;
 use std::io;
 use std::io::prelude::*;
@@ -7,11 +9,20 @@ use std::io::{Error, ErrorKind};
 use std::net::{TcpListener, TcpStream, SocketAddr};
 
 use std::collections::HashMap;
+use std::sync::mpsc;
+use std::sync::{Mutex, Arc};
+use std::time::Duration;
+use std::thread;
+use threadpool::ThreadPool;
 
 const DEFAULT_PORT: u16 = 8181;
+const NUM_WORKERS: usize = 4;
+
 const OP_GET: u8 = 0x00;
 const OP_PUT: u8 = 0x01;
 const OP_DEL: u8 = 0x02;
+
+type MapType = Arc<Mutex<HashMap<Vec<u8>, RcPackage>>>;
 
 // static mut THE_MAP: HashMap<[u8], [u8]> = HashMap::new();
 
@@ -26,9 +37,16 @@ struct RcPackage {
 }
 
 impl RcPackage {
+    fn val_len(&self) -> usize {
+        let keylen = self.data[0] as usize;
+        let val_len = self.data.len() - keylen - 1;
+        assert!(val_len == self.val().len());
+        val_len
+    }
+
     fn val(&self) -> &[u8] {
         let keylen = self.data[0] as usize;
-        return &self.data[keylen+1..];
+        &self.data[keylen+1..]
     }
 }
 
@@ -44,53 +62,83 @@ fn build_pkg(header: [u8; 3], data: Vec<u8>) -> RcPackage {
 fn main() -> io::Result<()> {
     let port = DEFAULT_PORT;
 
-    let mut map = HashMap::new();
+    let map = HashMap::new();
+    let maplock = Arc::new(Mutex::new(map));
+    let pool = ThreadPool::new(NUM_WORKERS);
+
+    // let (tx, rx) = mpsc::channel();
+    // let worker = thread::spawn(move || {
+    //     for stream in rx {
+    //         handle(&mut map, stream).expect("Error.");
+    //     }
+    // });
 
     println!("Listening on port {:}", port);
     let listener = TcpListener::bind(("localhost", port))?;
 
     for stream in listener.incoming() {
-        handle(&mut map, stream?).expect("Error.");
+        // handle(&mut map, stream?).expect("Error.");
+        // tx.send(stream?).unwrap();
+        if let Ok(stream) = stream {
+            {
+                let stream = &stream;
+                let peer = stream.peer_addr()?;
+                println!("Accepted connection from {:?}", peer);
+            }
+            let maplock = Arc::clone(&maplock);
+            pool.execute(move || {
+                match handle_conn(maplock, stream) {
+                    _ => println!("connection closed"),
+                };
+            });
+        }
     }
 
     Ok(())
 }
 
-fn handle(map: &mut HashMap<Vec<u8>, RcPackage>, mut stream: TcpStream) -> io::Result<()> {
-    println!("Accepted connection from {:?}", stream.peer_addr().expect("Error."));
-    let mut buf: [u8; 3] = [0; 3];
-    let bytes_read = stream.read(&mut buf)?;
-    if bytes_read < 3 {
-        return Err(Error::new(ErrorKind::Other, "Invalid message."));
-    }
-    println!("rcv ({:}): {:X?}", bytes_read, buf);
-    let len = (256 * buf[0] as u32 + buf[1] as u32) as usize;
-    println!("len = {:?}", len);
-    let mut msgbuf = Vec::with_capacity(len);
-    unsafe { msgbuf.set_len(len) };
-    let bytes_read = stream.read(&mut msgbuf[..])?;
-    unsafe { msgbuf.set_len(bytes_read) };
-    println!("rcv ({:}): {:X?}", bytes_read, msgbuf);
+fn handle_conn(map: MapType, mut stream: TcpStream) -> io::Result<()> {
+    loop {
+        println!("start parsing msg from {:?}", stream.peer_addr()?);
+        let mut buf: [u8; 3] = [0; 3];
+        let bytes_read = stream.read(&mut buf)?;
+        if bytes_read < 3 {
+            return Err(Error::new(ErrorKind::Other, "Incomplete message."));
+        }
+        println!("rcv ({:}): {:X?}", bytes_read, buf);
+        let len = (256 * buf[0] as u32 + buf[1] as u32) as usize;
+        println!("len = {:?}", len);
+        let mut msgbuf = Vec::with_capacity(len);
+        unsafe { msgbuf.set_len(len) };
+        let bytes_read = stream.read(&mut msgbuf[..])?;
+        unsafe { msgbuf.set_len(bytes_read) };
+        println!("rcv ({:}): {:X?}", bytes_read, msgbuf);
 
-    let pkg = build_pkg(buf, msgbuf);
+        let pkg = build_pkg(buf, msgbuf);
 
-    match buf[2] {
-        OP_GET => handle_get(map, stream, pkg),
-        OP_PUT => handle_put(map, stream, pkg),
-        // OP_DEL => handle_del(stream, &msgbuf[..]),
-        _ => return Err(Error::new(ErrorKind::Other, format!("Invalid OP: {:X?}", buf[2]))),
+        match buf[2] {
+            OP_GET => handle_get(&map, &stream, pkg)?,
+            OP_PUT => handle_put(&map, &stream, pkg)?,
+            // OP_DEL => handle_del(stream, &msgbuf[..]),
+            _ => return Err(Error::new(ErrorKind::Other, format!("Invalid OP: {:X?}", buf[2]))),
+        }
+        thread::sleep(Duration::from_millis(500));
     }
 }
 
-fn handle_get(map: &HashMap<Vec<u8>, RcPackage>, mut stream: TcpStream, pkg: RcPackage) -> io::Result<()> {
+fn handle_get(maplock: &MapType, mut stream: &TcpStream, pkg: RcPackage) -> io::Result<()> {
     println!("GET");
 
     let key = &pkg.data[..];
+    let map = maplock.lock().unwrap();
     match map.get(&key.to_vec()) {
         Some(valpkg) => {
-            println!("snd {} -> {}", desc(key), desc(valpkg.val()));
-            stream.write(valpkg.val())?;
-            stream.flush()?;
+            let val = valpkg.val();
+            println!("snd {} -> {}", desc(key), desc(val));
+            let val_len = &[valpkg.val_len() as u8];
+            println!("ie {:X?}{:X?}", &val_len, &val);
+            stream.write(val_len)?;
+            stream.write(val)?;
             Ok(())
         },
         None => {
@@ -100,13 +148,14 @@ fn handle_get(map: &HashMap<Vec<u8>, RcPackage>, mut stream: TcpStream, pkg: RcP
     }
 }
 
-fn handle_put(map: &mut HashMap<Vec<u8>, RcPackage>, stream: TcpStream, pkg: RcPackage) -> io::Result<()> {
+fn handle_put(maplock: &MapType, mut stream: &TcpStream, pkg: RcPackage) -> io::Result<()> {
     println!("PUT");
 
     let keylen = pkg.data[0] as usize;
-    let mut key = Vec::with_capacity(keylen);
-    unsafe { key.set_len(keylen) };
-    key.copy_from_slice(&pkg.data[1..1+keylen]);
+    // let mut key = Vec::with_capacity(keylen);
+    // unsafe { key.set_len(keylen) };
+    // key.copy_from_slice(&pkg.data[1..1+keylen]);
+    let  key = pkg.data[1..1+keylen].to_vec();
 
     // let msgbuf = &pkg.data[..];
     // let key = &msgbuf[1..keylen+1];
@@ -115,10 +164,13 @@ fn handle_put(map: &mut HashMap<Vec<u8>, RcPackage>, stream: TcpStream, pkg: RcP
     let insertstr = format!("inserted {:X?} -> {:X?}", desc(&key[..]), desc(&pkg.data[keylen+1..]));
     let updatestr = format!("updated {:X?} -> {:X?}", desc(&key[..]), desc(&pkg.data[keylen+1..]));
 
+    let mut map = maplock.lock().unwrap();
     match map.insert(key, pkg) {
         None => println!("{}", insertstr),
         _ => println!("{}", updatestr),
     }
+
+    stream.write(&[0x00])?;
 
     // stream.shutdown(Shutdown::Both);
 
--- src/send.py
+++ src/send.py
@@ -2,12 +2,25 @@
 
 import sys
 import socket
+import itertools
 
 addr = 'localhost'
 port = 8181
 
 def separate(bytes, window=1):
 	return ' '.join([ bytes[i:i+window].hex() for i in range(0, len(bytes), window) ])
+
+def parse_rsp_get(bs):
+	val_len = int(bs.__next__())
+	val = bytes(itertools.islice(bs, val_len))
+	# val = bs[1:val_len+1]
+	print(val.decode())
+	# return bs[val_len+1:]
+
+def parse_rsp_put(bs):
+	b = bs.__next__()
+	print('OK' if b == 0 else 'ERR {}'.format(b))
+	# return bs[1:]
 
 def send(bs):
 	print('sent: {}'.format(separate(bs)))
@@ -17,40 +30,64 @@ def send(bs):
 	s.send(bs)
 
 	s.settimeout(1)
-	try:
-		rsp = s.recv(128)
-		print('rcvd: {}'.format(separate(rsp)))
-	except:
-		print('timeout')
-
+	while True:
+		try:
+			for b in s.recv(128):
+				yield b
+		except:
+			break;
 	s.close()
 
+def raw(args):
+	return bytes.fromhex(''.join(args))
 
-subcmd = sys.argv[1].lower()
-
-if subcmd == 'raw':
-	bs = bytes.fromhex(''.join(sys.argv[2:]))
-	send(bs)
-elif subcmd == 'get':
-	key = sys.argv[2]
+def get(args):
+	key = args[0]
 	keybytes = key.encode()
 	size = len(keybytes)
 	msb = size // 256
 	lsb = size % 256
 	pkg = bytes([msb, lsb, 0]) + keybytes
-	send(pkg)
-elif subcmd == 'put':
-	key = sys.argv[2]
-	val = sys.argv[3]
+	return pkg, parse_rsp_get
+
+def put(args):
+	key = args[0]
+	val = args[1]
 	keybytes = key.encode()
 	valbytes = val.encode()
 	size = 1 + len(keybytes) + len(valbytes)
 	msb = size // 256
 	lsb = size % 256
 	pkg = bytes([msb, lsb, 1, len(keybytes)]) + keybytes + valbytes
-	send(pkg)
-else:
-	print('invalid subcommand')
+	return pkg, parse_rsp_put
+
+args = sys.argv[1:]
+to_send = b''
+to_parse = []
+
+while len(args) > 0:
+	subcmd = args[0].lower()
+
+	if subcmd == 'raw':
+		to_send += raw(args[1:2])
+		args = args[2:]
+	elif subcmd == 'get':
+		to_send_e, to_parse_e = get(args[1:2])
+		to_send += to_send_e
+		to_parse += [to_parse_e]
+		args = args[2:]
+	elif subcmd == 'put':
+		to_send_e, to_parse_e = put(args[1:3])
+		to_send += to_send_e
+		to_parse += [to_parse_e]
+		args = args[3:]
+	else:
+		print('invalid subcommand')
+
+rsp = send(to_send)
+for parser in to_parse:
+	parser(rsp)
+
 
 # for arg in sys.argv[1:]:
 # 	val = int(arg, 16);
